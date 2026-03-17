dc73fdbbb9fdd6a5f77ab5bacbe3308ff2c23fd5
Romain Pauly <plr5967a@ensfsi.onmicrosoft.com>
2026-03-17 08:34:35 +0100

    initialisation class gaulois et romain

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,26 @@
+package personnages;
+
+import java.io.ObjectInputStream.GetField;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendrearole() + "\"" + texte + "\"");
+	}
+
+	private String prendrearole() {
+		return "Le gaulois" + nom + " : ";
+	}
+
+}
