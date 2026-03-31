1cb6df964080ae5649f728764132e6356d48d7a3
Romain Pauly <plr5967a@ensfsi.onmicrosoft.com>
2026-03-31 09:38:06 +0200

    fin tp2 corretion

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,7 +1,5 @@
 package personnages;
 
-import java.security.PrivateKey;
-
 import objets.Equipement;
 
 public class Romain {
@@ -9,29 +7,15 @@ public class Romain {
 	private int force;
 	private Equipement[] tabEquipement;
 	private int nbEquipement = 0;
-	
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
-		assert isInvariantVerified(force);
 		tabEquipement = new Equipement[2];
-		
-		
+		assert isInvariantVerified(force);
+
 	}
 
-	public static void main(String[] args) {
-		Romain minus = new Romain("Minus", 6);
-		minus.sEquiper(Equipement.BOUCLIER);
-		minus.sEquiper(Equipement.BOUCLIER);
-		minus.sEquiper(Equipement.CASQUE);
-		minus.sEquiper(Equipement.CASQUE);
-		
-	    
-		
-	}
-	
-	
 	public String getNom() {
 		return nom;
 	}
@@ -44,52 +28,62 @@ public class Romain {
 	private String prendreParole() {
 		return "le romain " + this.nom + " : ";
 	}
+
 	private boolean isInvariantVerified(int force) {
-		if (force >=0) {
+		if (force >= 0) {
 			return true;
 		}
 		return false;
 	}
 
 	public void recevoirCoup(int forceCoup) {
-		assert forceCoup>=0;
+		assert forceCoup >= 0;
 		int temp = this.force;
 		this.force = this.force - forceCoup;
 		if (this.force < 1) {
 			parler("j'abandonne!");
-		} 
-		else {
+		} else {
 			parler("Aie");
 		}
 		assert isInvariantVerified(force);
-		assert temp>this.force;
+		assert temp > this.force;
 	}
-	
+
 	public void sEquiper(Equipement equipement) {
 		switch (this.nbEquipement) {
 		case 2: {
-			System.out.println("Le soldat" +nom+ "est déjà bien protégé !");
+			System.out.println("Le soldat" + nom + "est déjà bien protégé !");
 			break;
 		}
-		case 1:{
-			if(tabEquipement[0]==equipement) {
-				System.out.println("Le soldat "+ nom +" possède déjà un "+equipement+ "!");
-			}
-			else {
-				tabEquipement[1]=equipement;
-				System.out.println("Le soldat "+nom+" s’équipe avec un "+equipement+".");
-				nbEquipement++;
+		case 1: {
+			if (tabEquipement[0] == equipement) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement + "!");
+			} else {
+				ajouterEquipement(equipement);
 			}
 			break;
 		}
-		case 0:{
-			tabEquipement[0]=equipement;
-			System.out.println("Le soldat "+nom+" s’équipe avec un "+equipement+".");
-			nbEquipement++;
+		case 0: {
+			ajouterEquipement(equipement);
 			break;
 		}
 		default:
 			throw new IllegalArgumentException("Unexpected value: " + this.nbEquipement);
 		}
+	}
+
+	private void ajouterEquipement(Equipement equipement) {
+		tabEquipement[nbEquipement] = equipement;
+		System.out.println("Le soldat " + nom + " s’équipe avec un " + equipement + ".");
+		nbEquipement++;
+	}
+
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+
 	}
 }
--- src/village_gaulois/Village.java
+++ src/village_gaulois/Village.java
@@ -4,21 +4,53 @@ import personnages.Gaulois;
 
 public class Village {
 	private String nom;
+	private Gaulois[] tabVillage;
 	private int nbVillageois = 0;
 	private Gaulois chef;
-	private int nbVillageoisMax;
-	private Gaulois[] tabVilage;
-
 
 	public Village(String nom, Gaulois chef, int nbVillageoisMax) {
 		this.nom = nom;
 		this.chef = chef;
 		chef.setVillage(this);
-		this.nbVillageoisMax = nbVillageoisMax;
-		tabVilage = new Gaulois [nbVillageoisMax];
+		tabVillage = new Gaulois[nbVillageoisMax];
 	}
-	
-	
+
+	public String getNom() {
+		return nom;
+	}
+
+	public Gaulois getChef() {
+		return chef;
+	}
+
+	public void ajouterVillageois(Gaulois gaulois) {
+		tabVillage[nbVillageois] = gaulois;
+		nbVillageois++;
+		gaulois.setVillage(this);
+	}
+
+	public Gaulois trouverVillagois(int numVillageois) {
+		numVillageois--;
+		if (nbVillageois > numVillageois) {
+			return tabVillage[numVillageois];
+		} else {
+			System.out.println("Il n’y a pas autant d’habitants dans notre village !");
+			return null;
+		}
+
+	}
+
+	public void afficherVillageois() {
+		System.out.println("Dans le village \"" + nom + "\" du chef " + chef.getNom());
+		System.out.println("vivent les légendaires gaulois :");
+		for (Gaulois gaulois : tabVillage) {
+			if (gaulois != null) {
+				System.out.println("- " + gaulois.getNom());
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
 		Gaulois abraracourcix = new Gaulois("Abraracourcix", 6);
 		Village village = new Village("Village des Irréductibles", abraracourcix, 30);
@@ -37,43 +69,6 @@ public class Village {
 		asterix.sePresenter();
 		abraracourcix.sePresenter();
 		doublepolémix.sePresenter();
-	}
-
-
-	public String getNom() {
-		return nom;
-	}
-	public Gaulois getChef() {
-		return chef;
-	}
-	
-	public void ajouterVillageois(Gaulois gaulois) {
-		tabVilage[nbVillageois] = gaulois;
-		nbVillageois++;
-		gaulois.setVillage(this);
-	}
-	
-	
-	public Gaulois trouverVillagois(int numVillageois) {
-		numVillageois--;
-		if (nbVillageois>numVillageois) {
-			return tabVilage[numVillageois];
-		}
-		else {
-			System.out.println("Il n’y a pas autant d’habitants dans notre village !");
-			return null;
-		}
-		
-	}
-	public void afficherVillageois() {
-		System.out.println("Dans le village \""+nom+"\" du chef "+chef.getNom());
-		System.out.println("vivent les légendaires gaulois :");
-		for (Gaulois gaulois : tabVilage) {
-			if (gaulois != null) {
-				System.out.println("- "+gaulois.getNom());
-			}
-		}
-		
 	}
 
 }
