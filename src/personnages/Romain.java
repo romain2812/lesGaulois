191cb334365cd6259525c5864bc9d821a2f6546d
Romain Pauly <plr5967a@ensfsi.onmicrosoft.com>
2026-03-31 09:28:43 +0200

    es énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,17 +1,34 @@
 package personnages;
 
+import java.security.PrivateKey;
+
+import objets.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] tabEquipement;
+	private int nbEquipement = 0;
+	
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 		assert isInvariantVerified(force);
+		tabEquipement = new Equipement[2];
+		
+		
 	}
 
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		
+	    
+		
 	}
 	
 	
@@ -46,5 +63,33 @@ public class Romain {
 		}
 		assert isInvariantVerified(force);
 		assert temp>this.force;
+	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch (this.nbEquipement) {
+		case 2: {
+			System.out.println("Le soldat" +nom+ "est déjà bien protégé !");
+			break;
+		}
+		case 1:{
+			if(tabEquipement[0]==equipement) {
+				System.out.println("Le soldat "+ nom +" possède déjà un "+equipement+ "!");
+			}
+			else {
+				tabEquipement[1]=equipement;
+				System.out.println("Le soldat "+nom+" s’équipe avec un "+equipement+".");
+				nbEquipement++;
+			}
+			break;
+		}
+		case 0:{
+			tabEquipement[0]=equipement;
+			System.out.println("Le soldat "+nom+" s’équipe avec un "+equipement+".");
+			nbEquipement++;
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + this.nbEquipement);
+		}
 	}
 }
