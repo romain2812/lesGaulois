497852b5625a33d318288ae27de73609eb06ba2d
Romain Pauly <plr5967a@ensfsi.onmicrosoft.com>
2026-03-31 08:45:45 +0200

    invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,8 +7,14 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified(force);
 	}
 
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
+	}
+	
+	
 	public String getNom() {
 		return nom;
 	}
@@ -21,14 +27,24 @@ public class Romain {
 	private String prendreParole() {
 		return "le romain " + this.nom + " : ";
 	}
+	private boolean isInvariantVerified(int force) {
+		if (force >=0) {
+			return true;
+		}
+		return false;
+	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert forceCoup>=0;
+		int temp = this.force;
 		this.force = this.force - forceCoup;
 		if (this.force < 1) {
 			parler("j'abandonne!");
-		} else {
+		} 
+		else {
 			parler("Aie");
 		}
-
+		assert isInvariantVerified(force);
+		assert temp>this.force;
 	}
 }
